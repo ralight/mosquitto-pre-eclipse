8dd3cb2c9182282bfc87f9201a9e7519f074e621
Roger Light <roger@atchoo.org>
2010-01-13 18:00:58 +0000

    Install/uninstall targets for clients.

---
+++ config.mk
@@ -0,0 +1,18 @@
+VERSION=0.5pre
+TIMESTAMP:=$(shell date "+%F %T%z")
+
+#MANCOUNTRIES=en_GB
+
+CFLAGS=-I. -I.. -ggdb -Wall -O2
+LDFLAGS=-nopie -lsqlite3 -lwrap
+
+CC=gcc
+INSTALL=install
+XGETTEXT=xgettext
+MSGMERGE=msgmerge
+MSGFMT=msgfmt
+DOCBOOK2MAN=docbook2man.pl
+
+prefix=/usr/local
+mandir=${prefix}/share/man
+localedir=${prefix}/share/locale
