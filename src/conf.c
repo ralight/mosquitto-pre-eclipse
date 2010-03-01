33881964c0194a3b8243a62c5386e72e065209f9
Roger Light <roger@atchoo.org>
2010-03-01 10:18:56 +0000

    Add files to go in static binary tarballs for linux

---
+++ src/conf.c
@@ -0,0 +1,317 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <config.h>
+#include <mqtt3.h>
+
+int _mqtt3_conf_parse_bool(char **token, const char *name, bool *value);
+int _mqtt3_conf_parse_int(char **token, const char *name, int *value);
+
+void mqtt3_config_init(mqtt3_config *config)
+{
+	/* Set defaults */
+	config->autosave_interval = 1800;
+	config->daemon = false;
+#ifdef __CYGWIN__
+	config->ext_sqlite_regex = "./sqlite3-pcre.dll";
+#else
+	config->ext_sqlite_regex = "/usr/lib/sqlite3/pcre.so";
+#endif
+	config->iface = NULL;
+	config->iface_count = 0;
+	config->log_dest = MQTT3_LOG_STDERR;
+	config->log_type = MQTT3_LOG_ERR | MQTT3_LOG_WARNING | MQTT3_LOG_NOTICE | MQTT3_LOG_INFO;
+	config->persistence = false;
+	config->persistence_location = NULL;
+	config->pid_file = NULL;
+	config->retry_interval = 20;
+	config->sys_interval = 10;
+	config->user = "mosquitto";
+}
+
+int mqtt3_config_parse_args(mqtt3_config *config, int argc, char *argv[])
+{
+	int i;
+	int port_tmp;
+	char *str;
+
+	for(i=1; i<argc; i++){
+		if(!strcmp(argv[i], "-c") || !strcmp(argv[i], "--config-file")){
+			if(i<argc-1){
+				if(mqtt3_config_read(config, argv[i+1])){
+					mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Unable to open configuration file.");
+					return 1;
+				}
+			}else{
+				mqtt3_log_printf(MQTT3_LOG_ERR, "Error: -c argument given, but no config file specified.");
+				return 1;
+			}
+			i++;
+		}else if(!strcmp(argv[i], "-d") || !strcmp(argv[i], "--daemon")){
+			config->daemon = true;
+		}else if(!strcmp(argv[i], "-i") || !strcmp(argv[i], "--interface")){
+			if(i<argc-1){
+				i++;
+				str = strtok(argv[i], ":");
+				if(str){
+					config->iface_count++;
+					config->iface = mqtt3_realloc(config->iface, sizeof(struct mqtt3_iface)*config->iface_count);
+					if(!config->iface){
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Out of memory.");
+						return 1;
+					}
+					config->iface[config->iface_count-1].iface = mqtt3_strdup(str);
+					str = strtok(NULL, ":");
+					if(str){
+						port_tmp = atoi(str);
+						if(port_tmp < 1 || port_tmp > 65535){
+							mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid port value (%d).", port_tmp);
+							return 1;
+						}
+						config->iface[config->iface_count-1].port = port_tmp;
+					}else{
+						config->iface[config->iface_count-1].port = 1883;
+					}
+				}
+			}else{
+				mqtt3_log_printf(MQTT3_LOG_ERR, "Error: -i argument given, but no interface specifed.");
+				return 1;
+			}
+		}else if(!strcmp(argv[i], "-p") || !strcmp(argv[i], "--port")){
+			if(i<argc-1){
+				port_tmp = atoi(argv[i+1]);
+				if(port_tmp<1 || port_tmp>65535){
+					mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid port specified (%d).", port_tmp);
+					return 1;
+				}else{
+					config->iface_count++;
+					config->iface = mqtt3_realloc(config->iface, sizeof(struct mqtt3_iface)*config->iface_count);
+					if(!config->iface){
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Out of memory.");
+						return 1;
+					}
+					config->iface[config->iface_count-1].iface = NULL;
+					config->iface[config->iface_count-1].port = port_tmp;
+				}
+			}else{
+				mqtt3_log_printf(MQTT3_LOG_ERR, "Error: -p argument given, but no port specified.");
+				return 1;
+			}
+		}
+	}
+
+	if(!config->iface){
+		config->iface = mqtt3_malloc(sizeof(struct mqtt3_iface));
+		config->iface->iface = NULL;
+		config->iface->port = 1883;
+		config->iface_count = 1;
+	}
+	return 0;
+}
+
+int mqtt3_config_read(mqtt3_config *config, const char *filename)
+{
+	int rc = 0;
+	FILE *fptr = NULL;
+	char buf[1024];
+	char *token;
+	int port_tmp;
+	int log_dest = MQTT3_LOG_NONE;
+	int log_dest_set = 0;
+	int log_type = MQTT3_LOG_NONE;
+	int log_type_set = 0;
+	
+	fptr = fopen(filename, "rt");
+	if(!fptr) return 1;
+
+	while(fgets(buf, 1024, fptr)){
+		if(buf[0] != '#' && buf[0] != 10 && buf[0] != 13){
+			while(buf[strlen(buf)-1] == 10 || buf[strlen(buf)-1] == 13){
+				buf[strlen(buf)-1] = 0;
+			}
+			token = strtok(buf, " ");
+			if(token){
+				if(!strcmp(token, "autosave_interval")){
+					if(_mqtt3_conf_parse_int(&token, "autosave_interval", &config->autosave_interval)) return 1;
+					if(config->autosave_interval < 0) config->autosave_interval = 0;
+				}else if(!strcmp(token, "ext_sqlite_regex")){
+					token = strtok(NULL, " ");
+					if(token){
+						config->ext_sqlite_regex = mqtt3_strdup(token);
+					}else{
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Empty ext_sqlite_regex value in config.");
+						return 1;
+					}
+				}else if(!strcmp(token, "interface")){
+					token = strtok(NULL, " ");
+					if(token){
+						token = strtok(token, ":");
+						if(token){
+							config->iface_count++;
+							config->iface = mqtt3_realloc(config->iface, sizeof(struct mqtt3_iface)*config->iface_count);
+							if(!config->iface){
+								mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Out of memory.");
+								return 1;
+							}
+							config->iface[config->iface_count-1].iface = mqtt3_strdup(token);
+							token = strtok(NULL, ":");
+							if(token){
+								port_tmp = atoi(token);
+								if(port_tmp < 1 || port_tmp > 65535){
+									mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid port value (%d).", port_tmp);
+									return 1;
+								}
+								config->iface[config->iface_count-1].port = port_tmp;
+							}else{
+								config->iface[config->iface_count-1].port = 1883;
+							}
+						}
+					}else{
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Empty interface value in configuration.");
+						return 1;
+					}
+				}else if(!strcmp(token, "log_dest")){
+					token = strtok(NULL, " ");
+					if(token){
+						log_dest_set = 1;
+						if(!strcmp(token, "none")){
+							log_dest = MQTT3_LOG_NONE;
+						}else if(!strcmp(token, "syslog")){
+							log_dest |= MQTT3_LOG_SYSLOG;
+						}else if(!strcmp(token, "stdout")){
+							log_dest |= MQTT3_LOG_STDOUT;
+						}else if(!strcmp(token, "stderr")){
+							log_dest |= MQTT3_LOG_STDERR;
+						}else if(!strcmp(token, "topic")){
+							log_dest |= MQTT3_LOG_TOPIC;
+						}else{
+							mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid log_dest value (%s).", token);
+							return 1;
+						}
+					}else{
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Empty log_dest value in configuration.");
+						return 1;
+					}
+				}else if(!strcmp(token, "log_type")){
+					token = strtok(NULL, " ");
+					if(token){
+						log_type_set = 1;
+						if(!strcmp(token, "none")){
+							log_type = MQTT3_LOG_NONE;
+						}else if(!strcmp(token, "information")){
+							log_type |= MQTT3_LOG_INFO;
+						}else if(!strcmp(token, "notice")){
+							log_type |= MQTT3_LOG_NOTICE;
+						}else if(!strcmp(token, "warning")){
+							log_type |= MQTT3_LOG_WARNING;
+						}else if(!strcmp(token, "error")){
+							log_type |= MQTT3_LOG_ERR;
+						}else if(!strcmp(token, "debug")){
+							log_type |= MQTT3_LOG_DEBUG;
+						}else{
+							mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid log_type value (%s).", token);
+							return 1;
+						}
+					}else{
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Empty log_type value in configuration.");
+					}
+				}else if(!strcmp(token, "persistence")){
+					if(_mqtt3_conf_parse_bool(&token, "persistence", &config->persistence)) return 1;
+				}else if(!strcmp(token, "persistence_location")){
+					token = strtok(NULL, " ");
+					if(token){
+						config->persistence_location = mqtt3_strdup(token);
+						if(token[strlen(token)-1] != '/'){
+							mqtt3_log_printf(MQTT3_LOG_WARNING, "Warning: persistence_location should normally end with a '/'.");
+						}
+					}
+				}else if(!strcmp(token, "pid_file")){
+					token = strtok(NULL, " ");
+					if(token){
+						config->pid_file = mqtt3_strdup(token);
+					}
+				}else if(!strcmp(token, "port")){
+					if(_mqtt3_conf_parse_int(&token, "port", &port_tmp)) return 1;
+					if(port_tmp < 1 || port_tmp > 65535){
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid port value (%d).", port_tmp);
+						return 1;
+					}
+					config->iface_count++;
+					config->iface = mqtt3_realloc(config->iface, sizeof(struct mqtt3_iface)*config->iface_count);
+					if(!config->iface){
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Out of memory.");
+						return 1;
+					}
+					config->iface[config->iface_count-1].iface = NULL;
+					config->iface[config->iface_count-1].port = port_tmp;
+				}else if(!strcmp(token, "retry_interval")){
+					if(_mqtt3_conf_parse_int(&token, "retry_interval", &config->retry_interval)) return 1;
+					if(config->retry_interval < 1 || config->retry_interval > 3600){
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid retry_interval value (%d).", config->retry_interval);
+						return 1;
+					}
+				}else if(!strcmp(token, "sys_interval")){
+					if(_mqtt3_conf_parse_int(&token, "sys_interval", &config->sys_interval)) return 1;
+					if(config->sys_interval < 1 || config->sys_interval > 65535){
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid sys_interval value (%d).", config->sys_interval);
+						return 1;
+					}
+				}else if(!strcmp(token, "user")){
+					token = strtok(NULL, " ");
+					if(token){
+						config->user = mqtt3_strdup(token);
+					}else{
+						mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid user value.");
+						return 1;
+					}
+				}else{
+					mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Unknown configuration variable \"%s\".", token);
+					return 1;
+				}
+			}
+		}
+	}
+	fclose(fptr);
+
+	if(log_dest_set){
+		config->log_dest = log_dest;
+	}
+	if(log_type_set){
+		config->log_type = log_type;
+	}
+
+	return rc;
+}
+
+int _mqtt3_conf_parse_bool(char **token, const char *name, bool *value)
+{
+	*token = strtok(NULL, " ");
+	if(*token){
+		if(!strcmp(*token, "false") || !strcmp(*token, "0")){
+			*value = false;
+		}else if(!strcmp(*token, "true") || !strcmp(*token, "1")){
+			*value = true;
+		}else{
+			mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Invalid %s value (%s).", name, *token);
+		}
+	}else{
+		mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Empty %s value in configuration.", name);
+		return 1;
+	}
+	
+	return 0;
+}
+
+int _mqtt3_conf_parse_int(char **token, const char *name, int *value)
+{
+	*token = strtok(NULL, " ");
+	if(*token){
+		*value = atoi(*token);
+	}else{
+		mqtt3_log_printf(MQTT3_LOG_ERR, "Error: Empty %s value in configuration.", name);
+		return 1;
+	}
+
+	return 0;
+}
---
+++ src/context.c
@@ -0,0 +1,117 @@
+/*
+Copyright (c) 2009,2010, Roger Light <roger@atchoo.org>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. Neither the name of mosquitto nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+#include <config.h>
+#include <mqtt3.h>
+
+mqtt3_context *mqtt3_context_init(int sock)
+{
+	mqtt3_context *context;
+	struct sockaddr addr;
+	socklen_t addrlen;
+	char address[1024];
+
+	context = mqtt3_malloc(sizeof(mqtt3_context));
+	if(!context) return NULL;
+	
+	context->connected = false;
+	context->sock = sock;
+	context->last_msg_in = time(NULL);
+	context->last_msg_out = time(NULL);
+	context->keepalive = 60; /* Default to 60s */
+	context->clean_start = true;
+	context->id = NULL;
+
+	context->in_packet.payload = NULL;
+	mqtt3_context_packet_cleanup(&context->in_packet);
+	context->out_packet = NULL;
+
+	addrlen = sizeof(addr);
+	context->address = NULL;
+	if(!getpeername(sock, &addr, &addrlen)){
+		if(inet_ntop(AF_INET, &((struct sockaddr_in *)&addr)->sin_addr.s_addr, address, 1024)){
+			context->address = mqtt3_strdup(address);
+		}
+	}
+	
+	return context;
+}
+
+/* This should only be called from within mosquitto.c because that is the only
+ * place that can work with the context array. To cause a context to be cleaned
+ * in other places, call mqtt3_socket_close() instead. This will force the main
+ * loop in mosquitto.c to clean the context and act on clean_start as
+ * appropriate.
+ * This will result in any outgoing packets going unsent. If we're disconnected
+ * forcefully then it is usually an error condition and shouldn't be a problem,
+ * but it will mean that CONNACK messages will never get sent for bad protocol
+ * versions for example.
+ */
+void mqtt3_context_cleanup(mqtt3_context *context)
+{
+	if(!context) return;
+
+	if(context->sock != -1){
+		mqtt3_socket_close(context);
+	}
+	if(context->clean_start){
+		mqtt3_db_subs_clean_start(context->id);
+		mqtt3_db_messages_delete(context->id);
+		mqtt3_db_client_delete(context);
+	}
+	if(context->address) mqtt3_free(context->address);
+	if(context->id) mqtt3_free(context->id);
+	mqtt3_context_packet_cleanup(&(context->in_packet));
+	while(context->out_packet){
+		mqtt3_context_packet_cleanup(context->out_packet);
+		context->out_packet = context->out_packet->next;
+	}
+	mqtt3_free(context);
+}
+
+void mqtt3_context_packet_cleanup(struct _mqtt3_packet *packet)
+{
+	if(!packet) return;
+
+	/* Free data and reset values */
+	packet->command = 0;
+	packet->have_remaining = 0;
+	packet->remaining_count = 0;
+	packet->remaining_mult = 1;
+	packet->remaining_length = 0;
+	if(packet->payload) mqtt3_free(packet->payload);
+	packet->payload = NULL;
+	packet->to_process = 0;
+	packet->pos = 0;
+}
+
