562f94578a139c0ab24d65868cda3650e3e69366
Roger Light <roger@atchoo.org>
2011-05-04 00:02:35 +0100

    Remove client id limit in clients.

---
+++ config.mk
@@ -0,0 +1,20 @@
+# Also bump lib/mosquitto.h, lib/python/setup.py, CMakeLists.txt
+VERSION=0.10
+TIMESTAMP:=$(shell date "+%F %T%z")
+
+#MANCOUNTRIES=en_GB
+
+CFLAGS=-I. -I.. -ggdb -Wall -O2 -I../lib
+LDFLAGS=
+# Add -lwrap to LDFLAGS if compiling with tcp wrappers support.
+
+CC=gcc
+INSTALL=install
+XGETTEXT=xgettext
+MSGMERGE=msgmerge
+MSGFMT=msgfmt
+DOCBOOK2MAN=docbook2man.pl
+
+prefix=/usr/local
+mandir=${prefix}/share/man
+localedir=${prefix}/share/locale
