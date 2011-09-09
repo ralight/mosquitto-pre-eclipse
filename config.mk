bdf090f5cce04a311c775dcb8efa078dd1428879
Roger Light <roger@atchoo.org>
2011-09-09 22:11:49 +0100

    Add "replaceables" to all mosquitto.conf man page options.

---
+++ config.mk
@@ -0,0 +1,28 @@
+# Also bump lib/mosquitto.h, lib/python/setup.py, CMakeLists.txt
+VERSION=0.12.90
+TIMESTAMP:=$(shell date "+%F %T%z")
+
+#MANCOUNTRIES=en_GB
+
+CFLAGS=-I. -I.. -ggdb -Wall -O2 -I../lib -pg
+
+UNAME:=$(shell uname -s)
+ifeq ($(UNAME),QNX)
+	LIBS=-lsocket
+else
+	LIBS=
+endif
+
+LDFLAGS=-pg -nopie
+# Add -lwrap to LDFLAGS if compiling with tcp wrappers support.
+
+CC=gcc
+INSTALL=install
+XGETTEXT=xgettext
+MSGMERGE=msgmerge
+MSGFMT=msgfmt
+DOCBOOK2MAN=docbook2man.pl
+
+prefix=/usr/local
+mandir=${prefix}/share/man
+localedir=${prefix}/share/locale
